f7403b7a248c909da1fa96b86a4fdbf9e443ca99
Maoyin Sun <simonmysun@gmail.com>
2024-06-13 12:00:53 +0200

    fix traefik log parser: better time filter

--- log_parsers/nginx.sh
+++ log_parsers/nginx.sh
@@ -4,12 +4,20 @@ SINCE_MINUTES_AGO="${SINCE_MINUTES_AGO:-10}";
 LOG_PATH='/var/log/services/reverse-proxy/nginx/*';
 export TZ='Europe/Berlin';
 
-DATE_SINCE=$(date --date "-${SINCE_MINUTES_AGO} min" '+%d/%b/%Y:%T');
-while read -r line; do
-  timestamp=$(echo "$line" | awk '{print $4}' | tr -d '[' | tr -d ']' | awk -F'[: /]' '{print $2 " " $1 " " $3 " " $4 ":" $5 ":" $6 " " $7}');
-  host='0.0.0.0';
-  source_ip=$(echo "$line" | awk '{print $NF}' | tr -d '"');
-  service=nginx;
-  TZ='UTC' epoch_time=$(date -d "$timestamp" +%s);
-  echo "$epoch_time,$host,$source_ip,$service";
-done <<< $(cat $LOG_PATH | grep "] 404 \"" | awk -v d1=$DATE_SINCE '{gsub(/^[\[\t]+/, "", $4);}; $4 > d1');
+DATE_SINCE=$(date --date "-${SINCE_MINUTES_AGO} min" '+%s');
+cat $LOG_PATH | grep -v '\[error\]' | {
+  while read -r line; do
+    status=$(echo "$line" | awk '{print $6}');
+    if (( status < 400 )); then
+      continue;
+    fi
+    timestamp=$(echo "$line" | awk '{print $4 " " $5}' | tr -d '[' | tr -d ']' | awk -F'[: /]' '{print $2 " " $1 " " $3 " " $4 ":" $5 ":" $6 " " $7}') 
+    epoch_time=$(date -d "$timestamp" +%s);
+    if (( epoch_time < DATE_SINCE )); then
+      continue;
+    fi
+    source_ip=$(echo "$line" | awk '{print $NF}' | tr -d '"');
+    service=nginx;
+    echo "$epoch_time,$host,$source_ip,$service";
+  done
+};
